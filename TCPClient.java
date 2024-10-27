8c1da94935001ac704a7ae68f8888682ede7499b
Jalen <jalen10kk@gmail.com>
2024-10-27 19:40:13 -0400

    client updated

--- TCPClient.java
+++ TCPClient.java
@@ -4,17 +4,17 @@ import java.util.Random;
 
  public class TCPClient {
     public static void main(String[] args) throws IOException {
-       
-         // Variables for setting up connection and communication
+      
+      // Variables for setting up connection and communication
       Socket Socket = null; // socket to connect with ServerRouter
       PrintWriter out = null; // for writing to ServerRouter
       BufferedReader in = null; // for reading form ServerRouter
-         InetAddress addr = InetAddress.getLocalHost();
-         String host = addr.getHostAddress(); // Client machine's IP
-       String routerName = "172.20.10.3"; // ServerRouter host name (Last IP used was Jalen's on my IPhone)
-         int SockNum = 5555; // port number
-         
-         // Tries to connect to the ServerRouter
+      InetAddress addr = InetAddress.getLocalHost();
+      String host = addr.getHostAddress(); // Client machine's IP
+      String routerName = "172.20.10.3"; // ServerRouter host name (Last IP used was Jalen's on my IPhone)
+      int SockNum = 5555; // port number
+      
+      // Tries to connect to the ServerRouter
       try {
          Socket = new Socket(routerName, SockNum);
          out = new PrintWriter(Socket.getOutputStream(), true);
@@ -28,43 +28,43 @@ import java.util.Random;
             System.err.println("Couldn't get I/O for the connection to: " + routerName);
             System.exit(1);
          }
-             
-       // Variables for message passing	
+         
+      // Variables for message passing	
       int[][] tempMatrix = generateMatrix(1024, 1024);
       writeMatrixToFile(tempMatrix, "test2.txt");
       Reader reader = new FileReader("test2.txt"); 
-         BufferedReader fromFile =  new BufferedReader(reader); // reader for the string file
+      BufferedReader fromFile =  new BufferedReader(reader); // reader for the string file
       String fromServer; // messages received from ServerRouter
       String fromUser; // messages sent to ServerRouter
-         String address ="172.20.10.3"; // destination IP (Server) (Last IP used was Jalen's on my IPhone)
-         long t0, t1, t;
-         
-         // Communication process (initial sends/receives
-         out.println(address);// initial send (IP of the destination Server)
-         fromServer = in.readLine();//initial receive from router (verification of connection)
-         System.out.println("ServerRouter: " + fromServer);
-         out.println(host); // Client sends the IP of its machine as initial send
-         t0 = System.currentTimeMillis();
-       
-         // Communication while loop
+      String address ="172.20.10.3"; // destination IP (Server) (Last IP used was Jalen's on my IPhone)
+      long t0, t1, t;
+      
+      // Communication process (initial sends/receives
+      out.println(address);// initial send (IP of the destination Server)
+      fromServer = in.readLine();//initial receive from router (verification of connection)
+      System.out.println("ServerRouter: " + fromServer);
+      out.println(host); // Client sends the IP of its machine as initial send
+      t0 = System.currentTimeMillis();
+      
+      // Communication while loop
       while ((fromServer = in.readLine()) != null) {
          System.out.println("Server: " + fromServer);
-             t1 = System.currentTimeMillis();
+         t1 = System.currentTimeMillis();
          if (fromServer.equals("Bye.")) // exit statement
             break;
-             t = t1 - t0;
-             System.out.println("Cycle time: " + t);
+         t = t1 - t0;
+         System.out.println("Cycle time: " + t);
        
          fromUser = fromFile.readLine(); // reading strings from a file
          if (fromUser != null) {
             System.out.println("Client: ");
             out.println(fromUser); // sending the strings to the Server via ServerRouter
-                 t0 = System.currentTimeMillis();
+            t0 = System.currentTimeMillis();
          }
       }
 
-       
-         // closing connections
+      
+      // closing connections
       out.close();
       in.close();
       Socket.close();
